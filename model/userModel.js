4b161b4077395041e559da8cd8664c7f7ad24007
jessyelisius <jecintaelisius2020@gmail.com>
2025-03-14 16:10:05 +0100

    started model

---
+++ model/userModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const UserModel = new mongoose.Schema(
+  {
+    FirstName: {
+      type: String,
+      required: true,
+    },
+    LastName: {
+      type: String,
+      required: true,
+    },
+    Email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+        "invalid Email",
+      ],
+    },
+    Password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    Phone: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserModel);
