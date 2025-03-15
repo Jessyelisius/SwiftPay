2654faff65acecab99231089d871e86279533260
jessyelisius <jecintaelisius2020@gmail.com>
2025-03-15 12:35:09 +0100

    db done

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
+require("dotenv").config();
+const DBconnection = require("./config/dbconn");
+const morgan = require("morgan");
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT;
+DBconnection();
 
-app.listen(port, () => console.log(`app listening on port ${port}!`));
+////////////middleware////////
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.listen(port, () => console.log(`swiftPay app listening on port ${port}!`));
