9f2d444142401531cc79841c3056ac7b7bb003d8
jessyelisius <jecintaelisius2020@gmail.com>
2025-04-09 19:06:57 +0100

    registration complete

--- controller/AdminContrl/Register.js
+++ controller/AdminContrl/Register.js
@@ -1,5 +1,5 @@
 const adminModel = require("../../model/admin/admin.Model");
-
+const bcrypt = require('bcryptjs')
 
 const Register = async(req, res) =>{
         // Email regex pattern for basic email validation
@@ -17,8 +17,20 @@ const Register = async(req, res) =>{
         if(Input.Password?.length<6) return res.status(400).json({Error:true, Message:"Password is short min of 6 chars"});
         if(passwordRegex.test(Input.Password)) return res.status(400).json({Error:true, Message:"Password mush contain special chars"});
 
-        const existingUser = await adminModel.findOne()
+        const existingUser = await adminModel.findOne({Email:Input.Email});
+        if(!existingUser) return res.status(400).json({Error:true, Message:"Admin email already in user"});
+
+        const hashPwd = bcrypt.hash(Input.Password,10)
+        const user = await adminModel.create({
+            FirstName:Input.FirstName,
+            LastName:Input.LastName,
+            Email:Input.LastName,
+            Password:hashPwd
+        });
+
+        res.status(200).json({Error:false,Message:"registration successful", result:user})
     } catch (error) {
-        
+        console.log("error registrying user",error);
+        res.status(400).json({Error:true, Message:"cannot create user"})
     }
 }
