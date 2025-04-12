9e7c3f17c7457d82442bffc3860a3a0bfb35aced
jessyelisius <jecintaelisius2020@gmail.com>
2025-04-12 13:24:46 +0100

    update on routing

--- routes/AdminRoute/admin.js
+++ routes/AdminRoute/admin.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { Register, verifyEmail } = require('../../controller/AdminContrl/Register');
 const Login = require('../../controller/AdminContrl/Login');
+const { ForgetPassword, ResetPassword } = require('../../controller/AdminContrl/ForgetPassword');
 const router = express.Router();
 
 router.post('/register', Register);
 router.get('/verify-email', verifyEmail);
 router.post('/login', Login);
+router.post('/forgetPassword', ForgetPassword);
+router.post('/resetPassword', ResetPassword);
 
 module.exports = router;
