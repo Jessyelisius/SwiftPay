facc46089c2f03d47ac88f4289bf397cf73fad1c
jessyelisius <jecintaelisius2020@gmail.com>
2025-09-26 22:00:03 +0100

    feat: Add routes for USD virtual account creation and details retrieval

--- routes/walletRoute/wallet.js
+++ routes/walletRoute/wallet.js
@@ -5,11 +5,17 @@ const Transfer = require('../../controller/WalletPaymentContrl/Transfer');
 const { userBalance } = require('../../controller/WalletPaymentContrl/Balance');
 const { createWallet, getWallet, getBalance, updateBalance, hasSufficientBalance, addExternalWallet, getExternalWallet, getWalletSummary, testWalletOperations } = require('../../controller/CryptoCoin/walletService');
 const { validateWalletAddress, addExternalWalletWithValidation, processCryptoWithdrawal, calculateNetworkFee, getWithdrawalHistory, checkWithdrawalStatus, testCryptoTransferOperations } = require('../../controller/CryptoCoin/cryptoTransferService');
+const { CreateUsdVirtualAccount, getUsdVirtualAccountDetails } = require('../../controller/USD_Section/usdAccount');
 const router = express.Router();
 
 // Virtual Account routes
 router.post('/virtualAccountCreation', verifyUserJwt, CreateVirtualAccount);
 router.get('/getVirtualAccountDetails', verifyUserJwt, getVirtualAccountDetails);
+
+//usd virtual account 
+router.post('/createUsdVirtualAccount', verifyUserJwt, CreateUsdVirtualAccount);
+router.get('/getUsdVirtualAccountDetails', verifyUserJwt, getVirtualAccountDetails);
+
 
 //user balance route
 router.get('/balance', verifyUserJwt, userBalance);
